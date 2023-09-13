548b5ea73e3da354fff22c1d22d0c7731678adf2
John Davis <512jay@gmail.com>
2023-09-12 21:19:10 -0400

    Add privacy policy page

---
+++ react-tipper/src/components/Privacy.js
@@ -0,0 +1,12 @@
+// src/components/Privacy.js
+import React from 'react';
+
+const Privacy = () => {
+  return (
+    <div>
+      <h1>Privacy Page</h1>
+    </div>
+  );
+};
+
+export default Privacy;
